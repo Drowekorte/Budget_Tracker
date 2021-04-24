5ae98c04b24ec5e6a1fd33e6ac793152fb19ff39
Drowekorte <drowekorte@gmail.com>
2021-04-24 15:41:56 -0700

    Change PORT number

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 4000;
+const PORT = process.env.PORT || 5555;
 
 const app = express();
 
@@ -15,6 +15,10 @@ app.use(express.json());
 app.use(express.static("public"));
 // routes
 app.use(require("./routes/api.js"));
+// app.get("/", (req, res) => {
+//   console.log("go home")
+//   res.sendFile(__dirname + "/public/index.html")
+// })
 
 
 
